2aa818d76bebdf432f722d5bb3214efbaa8f4867
sudo-kp <petryk.hr@gmail.com>
2021-11-14 21:21:59 +0200

    aboba

--- fitnesssite/fitnesssite/settings.py
+++ fitnesssite/fitnesssite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'training.apps.TrainingConfig'
+    'training.apps.TrainingConfig',
+    'abonement.apps.AbonementConfig'
 ]
 
 MIDDLEWARE = [
--- fitnesssite/fitnesssite/urls.py
+++ fitnesssite/fitnesssite/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from abonement.views import index
 from training.views import home
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
+    path('abonement', index)
 ]
