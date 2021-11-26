2e86525c8cd2a303cd42a076957fdfa26da3e124
rraatt <rraatt12321@gmail.com>
2021-11-26 20:31:16 +0200

    Admin panel updates

--- fitnesssite/training/admin.py
+++ fitnesssite/training/admin.py
@@ -3,13 +3,17 @@ from rangefilter.filters import DateRangeFilter
 from .models import *
 
 
-
 class ScheduleAdmin(admin.ModelAdmin):
     list_display = ('id_trainer', 'id_client_group', 'date', 'time_start', 'time_end')
     list_filter = (('date', DateRangeFilter), 'content_type')
     search_fields = ('date', 'id_trainer__name')
 
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name', 'id_clients__name', 'id__clients__surname')
+
+
 admin.site.register(Schedule, ScheduleAdmin)
 admin.site.register(Training)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
--- fitnesssite/training/models.py
+++ fitnesssite/training/models.py
@@ -11,15 +11,19 @@ from abonement.models import Client, Employees
 class Schedule(models.Model):
     id_trainer = models.ForeignKey(Employees, on_delete=models.CASCADE, limit_choices_to={'profession': 't'})
     limit = models.Q(app_label='training', model='group') | models.Q(app_label='abonement', model='client')
-    content_type = models.ForeignKey(ContentType, limit_choices_to=limit, on_delete=models.CASCADE)
-    object_id = fields.GfkLookupField('content_type')
+    content_type = models.ForeignKey(ContentType, limit_choices_to=limit, on_delete=models.CASCADE, verbose_name='Type of training')
+    object_id = fields.GfkLookupField('content_type', verbose_name='Client/Group id')
     id_client_group = GenericForeignKey('content_type', 'object_id')
     date = models.DateField()
     time_start = models.TimeField()
     time_end = models.TimeField()
 
+    class Meta:
+        verbose_name = 'Schedule of training'
+        ordering = ['date']
+
     def __str__(self):
-        return f'{self.content_type}, {self.date}'
+        return f'{self.id_client_group}, {self.date}'
 
 
 class Training(models.Model):
@@ -28,8 +32,14 @@ class Training(models.Model):
     goal = models.TextField()
     achievements = models.TextField()
 
+    class Meta:
+        verbose_name = 'Training statistic'
+
 
 class Group(models.Model):
     name = models.CharField(max_length=50)
     id_clients = models.ManyToManyField(Client)
+
+    def __str__(self):
+        return self.name
 
