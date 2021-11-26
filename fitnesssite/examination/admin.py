5304e7b066eb8bf3f977d3b0f61f55f2cab25982
Illia Vodolieiev TI-01 <illuhavoda2003@gmail.com>
2021-11-26 20:15:34 +0200

    Revert "added models2"
    This reverts commit f1bea9580fe70bcf70b87c84b9bd66fda42eb2ff.

--- fitnesssite/examination/admin.py
+++ fitnesssite/examination/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from rangefilter.filters import DateRangeFilter
-from .models import *
 
-class ExaminationAdmin(admin.ModelAdmin):
-    list_display = ('date_examination', 'id_client', 'id_employ', 'weight', 'imt', 'fat_percentage', 'pressure', 'recommendation', 'result_of_examination')
-    search_fields = ('date_examination', 'id_client', 'id_employ')
-    list_filter = (('date_examination', DateRangeFilter), 'weight')
-
-admin.site.register(Examination, ExaminationAdmin)
-admin.site.register(Methodic)
+# Register your models here.
