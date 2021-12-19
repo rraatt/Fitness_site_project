9cc14dfe15e7607e9d27e42202e12511bba01308
Illia Vodolieiev TI-01 <illuhavoda2003@gmail.com>
2021-12-19 17:10:58 +0200

    added models2

--- fitnesssite/examination/views.py
+++ fitnesssite/examination/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from .models import *
 
-# Create your views here.
+def home(request):
+    return render(request, 'examination/home.html', {'title': 'Home page'})
+
+def about(request):
+    return render(request, 'examination/about.html', {'title': 'About'})
+
