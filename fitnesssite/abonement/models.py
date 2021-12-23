a08539eac3e1edbddc2cc7b5b6e16192433e3a9d
sudo-kp <petryk.hr@gmail.com>
2021-12-23 18:30:47 +0200

    models change

--- fitnesssite/abonement/models.py
+++ fitnesssite/abonement/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 
@@ -54,13 +58,23 @@ class Client(models.Model):
         ('w', 'woman'),
         ('t', 'they')
     ]
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    patronymic = models.CharField(max_length=50)
-    birthday = models.DateField()
-    sex = models.CharField(max_length=1, choices=SEX)
-    phone_number = models.IntegerField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=True)
+    surname = models.CharField(max_length=50, blank=True)
+    patronymic = models.CharField(max_length=50, blank=True)
+    birthday = models.DateField(null=True)
+    sex = models.CharField(max_length=1, choices=SEX, blank=True)
+    phone_number = models.IntegerField(null=True)
     description = models.TextField(blank=True)
 
     def __str__(self):
-        return f'{self.name} {self.surname}'
+        return f'{self.user} {self.name} {self.surname}'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Client.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.client.save()
--- fitnesssite/training/templatetags/training_tags.py
+++ fitnesssite/training/templatetags/training_tags.py
@@ -9,6 +9,5 @@ def show_menu():
     menu = [{'title': 'Make an appointment', 'url_name': 'new_training'},
             {'title': 'Group trainings', 'url_name': 'group_schedule'},
             {'title': 'Examinations', 'url_name': 'examination'},
-            {'title': 'About us', 'url_name': 'about'},
             {'title': 'Profile', 'url_name': 'profile'}]
     return {"menu": menu}
