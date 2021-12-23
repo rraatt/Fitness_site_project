051e03649f80482e17f47ec41d681e28960f02ab
rraatt <82118136+rraatt@users.noreply.github.com>
2021-12-23 14:38:11 +0200

    some views

--- fitnesssite/training/views.py
+++ fitnesssite/training/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, HttpResponse
 from django.views.generic import ListView
 
@@ -20,7 +21,39 @@ def about(request):
 
 
 def enlist(request, group_id):
-    return HttpResponse(f'You`ve joined group {group_id}')
+    current_user = request.user
+    if current_user.is_authenticated:
+        cur_client = Client.objects.get(user=current_user.id)
+        cur_group = Group.objects.get(pk=group_id)
+        cur_group.id_clients.add(cur_client)
+        return HttpResponse(f'You`ve joined group {group_id}')
+    else:
+        return HttpResponse(f'Please authorize!')
+
+
+class PersonalSchedule(ListView):
+    model = Schedule
+    extra_context = {'title': 'Your trainings'}
+    template_name = 'training/personal_schedule'
+    context_object_name = 'info'
+
+    def get_queryset(self):
+        current_user = self.request.user
+        cur_client = Client.objects.get(user=current_user)
+        return Schedule.objects.filter(client_group__client=cur_client)
+
+
+class PersonalGroup(ListView):
+    model = Schedule
+    extra_context = {'title': 'Your group trainigs'}
+    context_object_name = 'info'
+    template_name = 'training/schdule.html'
+
+    def get_queryset(self):
+        current_user = self.request.user
+        cur_client = Schedule.objects.get(user=current_user)
+        cur_groups = Group.objects.filter(id_clients=cur_client)
+        return Schedule.objects.filter(client_group__group__in=cur_groups)
 
 
 class GroupSchedule(ListView):
