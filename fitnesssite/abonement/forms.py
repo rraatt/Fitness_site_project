789105bb4158afc5f840e1cfd6be7af6683263b4
Illia Vodolieiev TI-01 <illuhavoda2003@gmail.com>
2021-12-29 13:55:57 +0200

    added captcha

--- fitnesssite/abonement/forms.py
+++ fitnesssite/abonement/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import *
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from captcha.fields import CaptchaField
 
 
 class BuyAbonement(forms.ModelForm):
@@ -36,6 +37,7 @@ class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    captcha = CaptchaField()
 
     class Meta:
         model = User
@@ -45,4 +47,5 @@ class RegisterUserForm(UserCreationForm):
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    captcha = CaptchaField()
 
